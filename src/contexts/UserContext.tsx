85ddc42ba1aed22ddbf5343f4eb657d3d4fcf56b
Winniebran <winnie_brandao@yahoo.com.br>
2022-11-02 16:42:27 -0300

    feat: add login context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { useState, createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const UserContext = () => {
+import { iUserLogin } from "../pages/Login/typeLogin";
+import { ApiRequests } from "../services/ApiRequest";
+import { iChildren, iUser, iUserContextProps } from "./typeContext";
+
+export const UserContext = createContext({} as iUserContextProps);
+
+export const UserProvider = ({ children }: iChildren) => {
+  const [user, setUser] = useState<iUser | null>(null);
+
+  const navigate = useNavigate();
+
+  const login = async (data: iUserLogin) => {
+    try {
+      const response = await ApiRequests.post("login", data);
+      localStorage.setItem("@astroverso:token", response.data.accessToken);
+      localStorage.setItem("@astroverso:id", response.data.user.id);
+      setUser(response.data.user);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>UserContext</div>
-  )
-}
+    <UserContext.Provider value={{ user, setUser, login }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
