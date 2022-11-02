2f4c834aa8849c962b2a5f6b9bb4b8c85b8c369c
FilipeC <filipecostasil77@gmail.com>
2022-11-02 13:44:51 -0300

    fix:PagesBug

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { NotFound } from '../components/NotFound/NotFound'
-import { ProtectRoutes } from '../components/ProtectRoutes/ProtectRoutes'
-import { Dashboard } from '../pages/Dashboard/Dashboard'
-import { Extras } from '../pages/Extras/Extras'
-import { Home } from '../pages/Home/Home'
-import { Login } from '../pages/Login/Login'
-import { Register } from '../pages/Register/Register'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "../components/NotFound/NotFound";
+import { ProtectRoutes } from "../components/ProtectRoutes/ProtectRoutes";
+import { Dashboard } from "../Pages/Dashboard/Dashboard";
+import { Extras } from "../Pages/Extras/Extras";
+import { Home } from "../Pages/Home/Home";
+import { Login } from "../Pages/Login/Login";
+import { Register } from "../Pages/Register/Register";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/">
-        <Route index element={<Home/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="extra" element={<Extras/>} />
-        <Route element={<ProtectRoutes/>}>
-          <Route path="dashboard" element={ <Dashboard/> } />
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="extra" element={<Extras />} />
+        <Route element={<ProtectRoutes />}>
+          <Route path="dashboard" element={<Dashboard />} />
         </Route>
       </Route>
-      <Route path="*" element={ <NotFound/> } />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
-}
+  );
+};
