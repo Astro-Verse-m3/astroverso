e85af28576cefbd0e631cf20935a7b0d26380119
Winniebran <winnie_brandao@yahoo.com.br>
2022-11-03 12:39:35 -0300

    refactor: typesStyles

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import { BaseTitle } from "./components/typography";
-import { iTextProps } from "./typesStyles/typeTitle";
+import { iTextProps } from "./typesStyles/typeTypography";
 
 export const StyledTitle = styled(BaseTitle)<iTextProps>`
+  //escrever aqui
+`;
 
-//escrever aqui 
+export const StyledSpan = styled(BaseTitle)<iTextProps>`
+  //escrever aqui
+`;
 
-`
+export const StyledParagraph = styled(BaseTitle)<iTextProps>`
+  //escrever aqui
+`;
