86a4b98129749bdb9ac8e2d8bbad9abc8cc89b43
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2022-11-05 10:30:35 -0300

    feat: add compoments and styles to header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Carousel } from "./Carousel/Carousel";
+import { Footer } from "./Footer/Footer";
+import { HeaderMenu } from "./HeaderMenu/HeaderMenu";
+import { StyledHomeContainer } from "./style";
 
 export const Home = () => {
-	return <div>Home</div>;
+	return (
+		<StyledHomeContainer>
+			<HeaderMenu />
+			<Carousel />
+			<Footer />
+		</StyledHomeContainer>
+	);
 };
