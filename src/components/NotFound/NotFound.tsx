8e2a339d5c5cb61838c8b0fe6803ce3482810a79
Winniebran <winnie_brandao@yahoo.com.br>
2022-11-07 11:53:34 -0300

    feat: add page notFound

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { StyledLink } from "../../styles/link";
+import { StyledSection } from "../../styles/section";
+import { StyledTitle } from "../../styles/typography";
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <StyledSection>
+      <StyledTitle tag="h2" fontSize="five" margin="one" editText="one">
+        Página não encontrada!
+      </StyledTitle>
+      <StyledLink to={"/"} type="two">
+        Cadastre-se!
+      </StyledLink>
+    </StyledSection>
+  );
+};
