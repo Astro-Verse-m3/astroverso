d84a4da01e29b78824a6dc39216dbfa9189ecfcb
kardashevd <106695001+tsukedev@users.noreply.github.com>
2022-11-06 22:44:25 -0300

    add: styledExtra.ts, typeExtra.tsx.

--- src/pages/Extras/Extras.tsx
+++ src/pages/Extras/Extras.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Header } from "../../components/Header/Header";
+import { StyledExtraUl } from "./styledExtra";
+import { iContents } from "./typeExtra";
 
 export const Extras = () => {
+  const [contents, setContents] = useState<iContents[]>([]);
+  const [contentSearch, setContentsSearch] = useState<string>("");
+
+  useEffect(() => {
+    const recomendations = async () => {
+      await axios
+        .get("https://astroverso-json-api.herokuapp.com/extras")
+        .then((response) => {
+          setContents(response.data);
+        })
+        .catch(() => {
+          console.log("Error");
+        });
+    };
+    recomendations();
+  }, []);
+
   return (
-    <div>Extras</div>
-  )
-}
+    <div>
+      <Header/>
+      <input
+        type="text"
+        placeholder="Digitar Pesquisa..."
+        onChange={(event) =>
+          setContentsSearch(event.target.value.toLowerCase())
+        }
+      />
+      <StyledExtraUl>
+        {contents.filter((content)=> {
+          if(contentSearch === ""){
+            return content
+          }
+          else if(content.title.toLowerCase().includes(contentSearch.toLowerCase())){
+           return content
+          }
+        }).map((content) => {
+          return (
+            <li key={content.id}>
+              <div>
+                <img src={content.image}/>
+              </div>
+              <div>
+                <h2>{content.title}</h2>
+                <span>{content.author}</span>
+                <p>{content.description}</p>         
+              </div>
+              <a target="_blank" href={content.link} rel="noreferrer">
+                Abrir na loja
+              </a>
+            </li>
+          );
+        })}
+      </StyledExtraUl>
+    </div>
+  );
+};
---
+++ src/pages/Extras/styledExtra.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledExtraUl = styled.ul`
+    background: linear-gradient(116.31deg, #2B2A70 0.49%, #191932 52.83%, #080808 100%);
+`
