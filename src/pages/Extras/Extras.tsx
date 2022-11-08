465142406da8569e147629dfa6b6e783df4bd7bf
kardashevd <106695001+tsukedev@users.noreply.github.com>
2022-11-08 10:33:43 -0300

    fix: Extras.tsx, styledExtra.ts.
    Adição do filtro para os botões, input de pesquisa. Estilização dos botões e do input.

--- src/pages/Extras/Extras.tsx
+++ src/pages/Extras/Extras.tsx
@@ -1,12 +1,30 @@
+import { Content } from "@radix-ui/react-dropdown-menu";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Header } from "../../components/Header/Header";
-import { StyledExtraUl } from "./styledExtra";
+import { HeaderMenu } from "../Home/HeaderMenu/HeaderMenu";
+import { StyledExtraDiv, StyledExtraUl } from "./styledExtra";
 import { iContents } from "./typeExtra";
 
 export const Extras = () => {
   const [contents, setContents] = useState<iContents[]>([]);
   const [contentSearch, setContentsSearch] = useState<string>("");
+  const [filterButton, setfilterButton] = useState(contents);
+
+  const handleButtons = (e: React.ChangeEvent<any>): void => {
+    let category = e.target.value;
+
+    if (category === 'Todos'){
+      setfilterButton(contents)
+    }
+    else if(category === 'Livros'){
+      const filtered = contents.filter(content=> content.type==='Livro')
+      setfilterButton(filtered);
+    }
+    else if (category === 'Filmes'){
+      const filtered = contents.filter(content=> content.type==='Filme')
+      setfilterButton(filtered);
+    }
+  }
 
   useEffect(() => {
     const recomendations = async () => {
@@ -23,41 +41,59 @@ export const Extras = () => {
   }, []);
 
   return (
-    <div>
-      <Header/>
-      <input
-        type="text"
-        placeholder="Digitar Pesquisa..."
-        onChange={(event) =>
-          setContentsSearch(event.target.value.toLowerCase())
-        }
-      />
+    <StyledExtraDiv>
+      <HeaderMenu />
+      <section>
+        <span>Recomendações</span>
+        <h1>Quer saber mais sobre Astronomia?</h1>
+      </section>
+      <div className="discoveryBox">
+        <div>
+          <button value="Livros" onClick={handleButtons}>Livros</button>
+          <button value="Filmes" onClick={handleButtons}>Filmes</button>
+          <button value="Todos" onClick={handleButtons}>Todos</button>
+        </div>
+        <input
+          type="text"
+          placeholder="Pesquisar na lista..."
+          onChange={(event) =>
+            setContentsSearch(event.target.value.toLowerCase())
+          }
+        />
+      </div>
       <StyledExtraUl>
-        {contents.filter((content)=> {
-          if(contentSearch === ""){
-            return content
-          }
-          else if(content.title.toLowerCase().includes(contentSearch.toLowerCase())){
-           return content
-          }
-        }).map((content) => {
-          return (
-            <li key={content.id}>
-              <div>
-                <img src={content.image}/>
-              </div>
-              <div>
-                <h2>{content.title}</h2>
-                <span>{content.author}</span>
-                <p>{content.description}</p>         
-              </div>
-              <a target="_blank" href={content.link} rel="noreferrer">
-                Abrir na loja
-              </a>
-            </li>
-          );
-        })}
+        {filterButton
+          .filter((content) => {
+            if (contentSearch === "") {
+              return content;
+            } else if (
+              content.title.toLowerCase().includes(contentSearch.toLowerCase())
+            ) {
+              return content;
+            }
+          })
+          .map((content) => {
+            return (
+              <li key={content.id}>
+                <div>
+                  <img src={content.image} />
+                </div>
+                <div>
+                  <div className="productContent">
+                    <h2>{content.title}</h2>
+                    <span>Por: {content.author}</span>
+                    <p>{content.description}</p>
+                  </div>
+                  <div className="linkContent">
+                    <a target="_blank" href={content.link} rel="noreferrer">
+                      Abrir na loja
+                    </a>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
       </StyledExtraUl>
-    </div>
+    </StyledExtraDiv>
   );
 };
--- src/pages/Extras/styledExtra.ts
+++ src/pages/Extras/styledExtra.ts
@@ -1,5 +1,190 @@
 import styled from "styled-components";
 
+export const StyledExtraDiv = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(
+    116.31deg,
+    #2b2a70 0.49%,
+    #191932 52.83%,
+    #080808 100%
+  );
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+
+  section {
+    font-family: var(--font-family-Raleway);
+    color: var(--color-grey-2);
+    font-size: var(--title-5);
+    margin-left: 50px;
+    margin-top: 30px;
+  }
+
+  section h1 {
+    font-family: var(--font-family-Raleway);
+    font-size: var(--title-3);
+    margin-top: 15px;
+    max-width: 250px;
+  }
+
+  .discoveryBox {
+    display: flex;
+    align-items: center;
+    margin-left: 50px;
+    flex-wrap: nowrap;
+    flex-direction: row;
+  }
+  .discoveryBox div {
+    border: 1.7px solid var(--color-grey-2);
+    border-radius: 50px;
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+  }
+
+  .discoveryBox div button {
+    color: var(--color-grey-2);
+    width: 70px;
+    border-radius: 50px;
+    height: 40px;
+    flex-wrap: nowrap;
+    :hover {
+      color: var(--color-grey-2);
+      transition: 1s;
+      background-color: #003b4a;
+    }
+  }
+
+  input {
+    font-family: var(--font-family-Raleway);
+    font-size: 18px;
+    background-color: transparent;
+    border-radius: 50px;
+    border: 1.7px solid var(--color-grey-2);
+    margin-top: 20px;
+    width: 210px;
+    height: 42px;
+    margin-left: 20px;
+    padding-left: 10px;
+    margin-bottom: 20px;
+  }
+
+  @media (max-width: 580px) {
+    flex-direction: column;
+    section {
+      margin-left: 20px;
+    }
+
+    .discoveryBox {
+      width: 210px;
+      margin-top: 10px;
+      margin-left: 20px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    input {
+      width: 210px;
+      height: 42px;
+      margin-top: 10px;
+      margin-left: 0px;
+      margin-right: 0px;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
 export const StyledExtraUl = styled.ul`
-    background: linear-gradient(116.31deg, #2B2A70 0.49%, #191932 52.83%, #080808 100%);
-`
+  max-width: 1200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  padding: 0px;
+  gap: 20px;
+
+  li {
+    width: 100%;
+    font-family: var(--font-family-Raleway);
+    border: solid var(--color-grey-1) 1px;
+    border-radius: 5px;
+    max-width: 340px;
+    height: 340px;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    padding-right: 10px;
+    gap: 10px;
+    color: var(--color-grey-1);
+  }
+
+  li .productContent {
+    height: 210px;
+    width: 200px;
+  }
+  li h2 {
+    color: var(--color-grey-1);
+    margin-bottom: 15px;
+    font-size: 15px;
+  }
+
+  li span {
+    width: 215px;
+    font-size: 14px;
+  }
+
+  li p {
+    height: 125px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 12px;
+    color: var(--color-grey-1);
+  }
+
+  li a {
+    font-family: var(--font-family-Raleway);
+    border-radius: 50px;
+    border: 1.7px solid var(--color-grey-2);
+    margin-top: 15px;
+    width: 140px;
+    height: 40px;
+    margin-left: 80px;
+    color: #003b4a;
+    background-color: var(--color-grey-2);
+
+    padding: 10px;
+    :hover {
+      color: var(--color-grey-2);
+      transition: 1s;
+      background-color: #003b4a;
+    }
+  }
+
+  li img {
+    width: 130px;
+    border-radius: 5px;
+    height: 200px;
+    border: none;
+    border-style: none;
+    border-color: transparent;
+  }
+
+  @media (max-width: 370px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    li {
+      width: 80%;
+      height: 480px;
+      display: flex;
+      flex-direction: column;
+      padding-top: 22px;
+    }
+    li .linkContent {
+      margin-left: 0px;
+    }
+  }
+`;
