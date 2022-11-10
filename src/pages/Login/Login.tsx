a8877fd84faac833d941bb570e483353adc72dca
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2022-11-10 00:11:56 -0300

    importing modal to check style

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,6 @@
 import Astronaut from "../../assets/Astronaut.gif";
 import { Header } from "../../components/Header/Header";
+import { Modal } from '../../components/Modal/Modal';
 import { StyledSection } from "../../styles/section";
 import { StyledTitle } from "../../styles/typography";
 import { FormLogin } from "./FormLogin";
@@ -11,12 +12,13 @@ export const Login = () => {
         <Header />
         <div className="header-gif">
           <img src={Astronaut} alt="gif de astronauta" />
-          <StyledTitle tag="span" fontSize="five" margin="one" editText="one">
+          <StyledTitle tag="span" fontSize="five" margin="one">
             Astronomia perto de você
           </StyledTitle>
         </div>
       </div>
       <FormLogin />
+      <Modal />
     </StyledSection>
   );
 };
