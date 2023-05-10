c64dd00b7a338f5f86c5a54966cd050e2a26f384
Winnie Brandão <106097591+Winniebran@users.noreply.github.com>
2023-05-10 23:17:32 +0100

    Update ApiRequest.tsx

--- src/services/ApiRequest.tsx
+++ src/services/ApiRequest.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("@astroverso:token");
 export const ApiRequests = axios.create({
   baseURL: "https://astroverso-json-api.onrender.com/",
-  timeout: 5000,
+  timeout: 150000,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
